eb35063d8c7f04be40af5970f85f2fcccbfd6a86
Diogo Righi <diogorighi@gmail.com>
2016-06-10 15:16:48 -0300

    yelp config

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+var Yelp = require('yelp');
 
-/* GET home page. */
+var yelp = new Yelp({
+  consumer_key: '',
+  consumer_secret: '',
+  token: '',
+  token_secret: '',
+});
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/search/:location', function(req, res){
+  var location = req.params.location;
+  yelp.search({ term: 'food', location: location })
+    .then(function (data) {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
 });
 
 module.exports = router;
